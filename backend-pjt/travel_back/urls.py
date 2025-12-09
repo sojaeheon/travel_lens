e7007c0ca1621154b8209efb34afdcb8fec64a32
소재헌 <sojaeheon123@gmail.com>
2025-12-09 21:32:48 +0900

    feat(TL-21): Swagger 셋팅

--- backend-pjt/travel_back/urls.py
+++ backend-pjt/travel_back/urls.py
@@ -15,9 +15,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Travel Lens API",
+        default_version="v1",
+        description="Travel Lens 백엔드 Swagger 문서",
+        contact=openapi.Contact(email="example@mail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    
+    # ================================
+    # SWAGGER 관련 URL
+    # ================================
+    # 🔥 Swagger UI
+    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-ui"),
+
+    # 🔥 ReDoc (옵션)
+    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="redoc-ui"),
+
+    # ================================
+    # 서버 관련 URL
+    # ================================
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
 ]
